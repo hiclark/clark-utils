fa959a158590e2cf5f742b85e744a1cd0f76a3c0
Ryan Boland <bolandryanm@gmail.com>
2020-03-26 15:00:59 -0400

    handle null format dollars

--- src/currency/index.js
+++ src/currency/index.js
@@ -5,19 +5,27 @@ import { roundToTwoDecimalPlaces, roundToZeroDecimalPlaces } from '../round';
 const numberWithCommas = (number: number): string =>
   new Big(number).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 
-export const centsToDollars = (cents: number): number =>
-  roundToTwoDecimalPlaces(new Big(cents / 100));
-
-export const dollarsToCents = (dollars: number): number =>
-  roundToZeroDecimalPlaces(new Big(dollars * 100));
-
-export const formatCentsToDollars = (cents: ?number): string => {
-  if (!cents) return '$—'
-  return `$${numberWithCommas(centsToDollars(cents))}`;
+const nullOrUndefined = (x: any): boolean => {
+  return (typeof x === 'undefined') || x === null
 }
 
-export const formatDollars = (dollars: number): string =>
-  `$${numberWithCommas(dollars)}`;
+export const centsToDollars = (cents: number): number => {
+  return roundToTwoDecimalPlaces(new Big(cents / 100));
+}
+
+export const dollarsToCents = (dollars: number): number => {
+  return roundToZeroDecimalPlaces(new Big(dollars * 100));
+}
+
+export const formatCentsToDollars = (cents: ?number): string => {
+  if (nullOrUndefined(cents)) return '$—'
+  return `$${numberWithCommas(centsToDollars(cents || 0))}`;
+}
+
+export const formatDollars = (dollars: ?number): string => {
+  if (nullOrUndefined(dollars)) return '$—'
+  return `$${numberWithCommas(dollars || 0)}`;
+}
 
 export const formatDollarsPerHour = (dollarsPerHour: number): string =>
   `${formatDollars(dollarsPerHour)}/hr`;
